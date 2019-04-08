2beb92ddb940bb77826787aaf16390d75413ab6f
fernando-albsilva <fernando.albsilva@gmail.com>
2019-04-07 22:02:55 -0300

    segundo commit

--- exemplo_matriz_dinamica_alocada_com_vertor_ponteiros.c
+++ exemplo_matriz_dinamica_alocada_com_vertor_ponteiros.c
@@ -1,80 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-void read_mat_size(int*,int*);
-void aloc_mat(int*,int*,int**,int*);
-//void read_mat(int*,int*,int**);
-
+void read_mat_size(int *, int *);
+int **aloc_mat(int **, int, int);
+void read_mat(int *, int *, int **);
+void print_mat(int *, int *, int **);
 int main()
 {
-    int linha,coluna;
-    int *p_linha,*p_coluna,**mat_p,*mat;
-    int i,j;
-    p_linha=&linha;
-    p_coluna=&coluna;
+    int linha, coluna;
+    int *p_linha, *p_coluna, **mat_p;
+    int i, j;
+    p_linha = &linha;
+    p_coluna = &coluna;
 
+    read_mat_size(p_linha, p_coluna);
+    mat_p = aloc_mat(mat_p, linha, coluna);
+    printf("%d - %d", linha, coluna);
+    read_mat(p_linha, p_coluna, mat_p);
+    print_mat(p_linha, p_coluna, mat_p);
 
-    read_mat_size(p_linha,p_coluna);
-    aloc_mat(p_linha,p_coluna,mat_p,mat);
-    
-    //read_mat(p_linha,p_coluna,mat);
-for (i=0; i<(*p_linha); i++)
-    {
-       
-            printf("%d",mat[i]);
-            
-        
-    }
-    
     return 0;
 }
 
-void read_mat_size(int*p_linha,int *p_coluna)
+void read_mat_size(int *p_linha, int *p_coluna)
 {
     printf("\n Digite o total de linhas da matriz :");
-    scanf("%d",p_linha);
+    scanf("%d", p_linha);
     printf("\n Digite o tamanho de colunas da matriz :");
-    scanf("%d",p_coluna);
-
-    //mat=(int**)malloc((*p_linha)*sizeof(int*);
-
-
-
+    scanf("%d", p_coluna);
 }
-void aloc_mat(int* p_linha, int* p_coluna, int **mat_p,int *mat)
+int **aloc_mat(int **mat_p, int linha, int coluna)
 {
-    int i,j;
-    mat_p=(int **)malloc((*p_linha)*sizeof(int *));
-    mat=(int *)malloc((*p_coluna) * sizeof(int) );
-        for (i=0;i<(*p_linha);i++)
-        {
-            mat_p[i]=mat;
-        }
-        
-  for (i=0; i<(*p_linha); i++)
+    int i;
+
+    mat_p = (int **)malloc(linha * sizeof(int *));
+    if (mat_p == NULL)
     {
-        for(j=0; j<(*p_coluna); j++)
+        printf("\n Nao há memoria suficiente \n");
+        free(mat_p);
+        return 0;
+    }
+    for (i = 0; i < linha; i++)
+    {
+        mat_p[i] = (int *)malloc(coluna * sizeof(int));
+        if (mat_p[i] == NULL)
         {
-            printf("\n Digite mat[%d][%d] =",i,j);
-            scanf("%d",&mat[i]);
+            printf("\n Nao há memoria suficiente \n");
+            free(mat_p[i]);
+            return 0;
+        }
+    }
+    return mat_p;
+}   
+
+void read_mat(int *p_linha, int *p_coluna, int **mat)
+{
+    int i, j;
+    for (i = 0; i < (*p_linha); i++)
+    {
+        for (j = 0; j < (*p_coluna); j++)
+        {
+            printf("\n Digite mat[%d][%d] =", i, j);
+            scanf("%d", &mat[i][j]);
         }
     }
 }
 
-
-
-/*void read_mat(int *p_linha,int*p_coluna,int **mat)
+void print_mat(int *p_linha, int *p_coluna, int **mat)
 {
-    int i,j;
-    for (i=0; i<(*p_linha); i++)
-    {
-        for(j=0; j<(*p_coluna); j++)
-        {
-            printf("\n Digite mat[%d][%d] =",i,j);
-            scanf("%d",&mat[i][j]);
-        }
-    }
+    int i, j;
 
+    for (i = 0; i < (*p_linha); i++)
+    {
+        for (j = 0; j < (*p_coluna); j++)
+        {
+            printf("%5d", mat[i][j]);
+        }
+        printf("\n");
+    }
 }
-*/
